a260b75763ba2a09c827f23ea26b589fc199e868
hoangson33 <son.pnh38@aptechlearning.edu.vn>
2021-12-31 21:57:26 +0700

    first commit

---
+++ BookingOnline/src/main/java/com/demo/BookingOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingOnlineApplication.class, args);
+	}
+
+}
